53ecd980f8cec7cb611aac56946ae06019fa72a6
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-03-06 11:27:03 -0500

    fix import

--- plop/actions/TheGoodStore/index.js
+++ plop/actions/TheGoodStore/index.js
@@ -1,27 +1,32 @@
-const { spawn } = require('child_process');
+import { spawn } from "child_process";
 
 export default function getTheGoodStoreAction(_, config) {
-	const spawnOptions = config.verbose ? {
-		cwd: config.path,
-		shell: true,
-		stdio: 'inherit',
-	} : {
-		cwd: config.path
-	};
+  const spawnOptions = config.verbose
+    ? {
+        cwd: config.path,
+        shell: true,
+        stdio: "inherit",
+      }
+    : {
+        cwd: config.path,
+      };
 
+  const gitApply = () =>
+    new Promise((resolve, reject) => {
+      const gitAdd = spawn(
+        "git",
+        ["apply", "/patches/good-store.patch"],
+        spawnOptions
+      );
 
-	const gitApply = () =>
-		new Promise((resolve, reject) => {
-			const gitAdd = spawn('git', ['apply', '/patches/good-store.patch'], spawnOptions);
+      gitAdd.on("close", (code) => {
+        if (didSucceed(code)) {
+          resolve(`git commit ran correctly`);
+        } else {
+          reject(`git add exited with ${code}`);
+        }
+      });
+    });
 
-			gitAdd.on('close', (code) => {
-				if (didSucceed(code)) {
-					resolve(`git commit ran correctly`);
-				} else {
-					reject(`git add exited with ${code}`);
-				}
-			});
-		});
-
-	return gitApply();
+  return gitApply();
 }
