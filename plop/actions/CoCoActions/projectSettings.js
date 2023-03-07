76b2f8268fdf303a2b97d62385a8041977519d06
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-03-07 09:13:22 -0500

    use authUrl and hostURL instead of hardcoded urls

--- plop/actions/CoCoActions/projectSettings.js
+++ plop/actions/CoCoActions/projectSettings.js
@@ -1,58 +1,62 @@
 import locales from "../../locales.js";
 
 const getProjectSettings = (plop, data, baseApi, projectSettings) => {
-    plop.setActionType("setupConnection", function (answers) {
-        return baseApi.initialize(
-            answers.clientId,
-            answers.clientSecret,
-            answers.projectName
-        );
-    });
+  plop.setActionType("setupConnection", function (answers) {
+    return baseApi.initialize(
+      answers.clientId,
+      answers.clientSecret,
+      answers.projectName,
+      answers.authUrl,
+      answers.hostUrl
+    );
+  });
 
-    plop.setActionType("getProjectSettings", function (answers, _, plop) {
-        return baseApi.getProjectSettings().then((res) => {
-            projectSettings = res;
-            return plop.inquirer
-                .prompt({
-                    type: "checkbox",
-                    name: "countries",
-                    message: "Select countries",
-                    choices: locales
-                        .map((co) => ({
-                            name: co.alpha2,
-                            checked: res.countries.includes(co.alpha2),
-                        }))
-                        .sort((a) => (a.checked ? -1 : 1)),
-                })
-                .then(({ countries }) => {
-                    answers.countries = countries;
+  plop.setActionType("getProjectSettings", function (answers, _, plop) {
+    return baseApi.getProjectSettings().then((res) => {
+      projectSettings = res;
+      return plop.inquirer
+        .prompt({
+          type: "checkbox",
+          name: "countries",
+          message: "Select countries",
+          choices: locales
+            .map((co) => ({
+              name: co.alpha2,
+              checked: res.countries.includes(co.alpha2),
+            }))
+            .sort((a) => (a.checked ? -1 : 1)),
+        })
+        .then(({ countries }) => {
+          answers.countries = countries;
 
-                    return plop.inquirer
-                        .prompt({
-                            type: "list",
-                            name: "defaultCountry",
-                            message: "Select the default country",
-                            choices: countries,
-                        })
-                        .then(({ defaultCountry }) => {
-                            const defaultCountryLocale = locales.find(locale => locale.alpha2 === defaultCountry)
-                            answers.defaultCountry = defaultCountryLocale;
-                            return defaultCountry;
-                        });
-                });
+          return plop.inquirer
+            .prompt({
+              type: "list",
+              name: "defaultCountry",
+              message: "Select the default country",
+              choices: countries,
+            })
+            .then(({ defaultCountry }) => {
+              const defaultCountryLocale = locales.find(
+                (locale) => locale.alpha2 === defaultCountry
+              );
+              answers.defaultCountry = defaultCountryLocale;
+              return defaultCountry;
+            });
         });
     });
+  });
 
-    return [
-        {
-            data,
-            type: "setupConnection",
-        },
-        {
-            data,
-            type: "getProjectSettings",
-        },
-    ];
+  return [
+    {
+      data,
+      type: "setupConnection",
+    },
+    {
+      data,
+      type: "getProjectSettings",
+    },
+  ];
 };
 
 export default getProjectSettings;
--- plop/utils/BaseApi.js
+++ plop/utils/BaseApi.js
@@ -1,38 +1,44 @@
 import fetch from "node-fetch";
 
 class BaseApi {
-    initialize = async (clientId, clientSecret, projectName) => {
-        this.projectName = projectName;
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-        await this.getAccessToken();
-    };
+  initialize = async (
+    clientId,
+    clientSecret,
+    projectName,
+    authUrl,
+    hostUrl
+  ) => {
+    this.projectName = projectName;
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.clientSecret = clientSecret;
+    this.authUrl = authUrl;
+    this.hostUrl = hostUrl;
+    await this.getAccessToken();
+  };
 
-    getAccessToken = async () => {
-        const res = await fetch(
-            `https://auth.us-central1.gcp.commercetools.com/oauth/token?grant_type=client_credentials&scope=manage_project:${this.projectName}`,
-            {
-                method: "post",
-                headers: {
-                    Authorization: `Basic ${Buffer.from(
-                        `${this.clientId}:${this.clientSecret}`
-                    ).toString("base64")}`,
-                },
-            }
-        ).then((res) => res.json());
-        this.token = res.access_token;
-    };
+  getAccessToken = async () => {
+    const res = await fetch(
+      `${this.authUrl}/oauth/token?grant_type=client_credentials&scope=manage_project:${this.projectName}`,
+      {
+        method: "post",
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${this.clientId}:${this.clientSecret}`
+          ).toString("base64")}`,
+        },
+      }
+    ).then((res) => res.json());
+    this.token = res.access_token;
+  };
 
-    getProjectSettings = async () => {
-        return fetch(
-            `https://api.us-central1.gcp.commercetools.com/${this.projectName}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${this.token}`,
-                },
-            }
-        ).then(res=>res.json());
-    };
+  getProjectSettings = async () => {
+    return fetch(`${this.hostUrl}/${this.projectName}`, {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    }).then((res) => res.json());
+  };
 }
 
 export default BaseApi;
